92e7e5c972f5a932f6e66e6c2fdcc42c0b09a50c
김민재 <joshuara7235@gmail.com>
2022-02-18 17:01:26 +0900

    Init git project -> practice for domain design

---
+++ src/main/java/me/joshua/ming_board_villege/MingBoardVillegeApplication.java
@@ -0,0 +1,13 @@
+package me.joshua.ming_board_villege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingBoardVillegeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MingBoardVillegeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/joshua/ming_board_villege/MingBoardVillegeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.joshua.ming_board_villege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingBoardVillegeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
