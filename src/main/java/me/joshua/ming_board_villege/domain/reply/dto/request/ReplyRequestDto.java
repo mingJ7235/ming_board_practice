7f6ff8fb345c0a34db45da0366654b9c655caae5
김민재 <joshuara7235@gmail.com>
2022-02-26 12:06:34 +0900

    Reply dto response design

--- src/main/java/me/joshua/ming_board_villege/domain/reply/dto/request/ReplyRequestDto.java
+++ src/main/java/me/joshua/ming_board_villege/domain/reply/dto/request/ReplyRequestDto.java
@@ -15,7 +15,6 @@ public class ReplyRequestDto {
 
         private Long writerId;
 
-
     }
 
 }
---
+++ src/main/java/me/joshua/ming_board_villege/domain/reply/dto/response/ReplyResponseDto.java
@@ -0,0 +1,38 @@
+package me.joshua.ming_board_villege.domain.reply.dto.response;
+
+import lombok.*;
+import me.joshua.ming_board_villege.domain.reply.entity.Reply;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor (access = AccessLevel.PRIVATE)
+public class ReplyResponseDto {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor (access = AccessLevel.PRIVATE)
+    @NoArgsConstructor (access = AccessLevel.PRIVATE)
+    public static class Response {
+        private Long id;
+
+        private Long memberId;
+
+        private String content;
+
+        private LocalDateTime createdTime;
+
+        private LocalDateTime updatedTime;
+
+        public static Response from (final @NotNull Reply reply) {
+            return Response.builder()
+                    .id(reply.getId())
+                    .memberId(reply.getMember().getId())
+                    .content(reply.getContent())
+                    .createdTime(reply.getCreatedTime())
+                    .updatedTime(reply.getUpdatedTime())
+                    .build();
+        }
+    }
+
+}
