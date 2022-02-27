bcadfb0355a41497d12abb64ae2eeef2d92e4479
김민재 <joshuara7235@gmail.com>
2022-02-27 22:40:24 +0900

    Transactional option modified

--- src/main/java/me/joshua/ming_board_villege/domain/reply/service/core/ReplyService.java
+++ src/main/java/me/joshua/ming_board_villege/domain/reply/service/core/ReplyService.java
@@ -2,8 +2,6 @@ package me.joshua.ming_board_villege.domain.reply.service.core;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.joshua.ming_board_villege.domain.reply.dto.request.ReplyRequestDto;
-import me.joshua.ming_board_villege.domain.reply.dto.response.ReplyResponseDto;
 import me.joshua.ming_board_villege.domain.reply.entity.Reply;
 import me.joshua.ming_board_villege.domain.reply.repository.ReplyRepository;
 import org.springframework.stereotype.Service;
@@ -13,19 +11,20 @@ import javax.validation.constraints.NotNull;
 
 @Slf4j
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 public class ReplyService {
 
     private final ReplyRepository replyRepository;
 
+    public Reply save (final @NotNull Reply reply) {
+        return replyRepository.save(reply);
+    }
+
+    @Transactional (readOnly = true)
     public Reply findById(final @NotNull Long id) {
         return replyRepository.findById(id)
                         .orElseThrow(() -> new IllegalArgumentException("not found reply id"));
-    }
-
-    public Reply save (final @NotNull Reply reply) {
-        return replyRepository.save(reply);
     }
 
 }
